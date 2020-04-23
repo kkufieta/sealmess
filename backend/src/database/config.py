5655e7d880f1ce65c6b205a4aef37b6325fdaa53
kkufieta <katharina.kufieta@gmail.com>
2020-04-23 15:52:46 -0400

    Make Heroku work

--- backend/src/database/config.py
+++ backend/src/database/config.py
@@ -3,13 +3,18 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 project_dir = os.path.dirname(os.path.abspath(__file__))
 
-database_name = "sealmess"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
-
 # Enable debug mode.
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = database_path
+if not os.environ.get('DATABASE_URL'):
+    print('can not find Herokus postgres url')
+    database_name = "sealmess"
+    database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+    SQLALCHEMY_DATABASE_URI = database_path
+else:
+    print('Found Herokus postgres url')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
