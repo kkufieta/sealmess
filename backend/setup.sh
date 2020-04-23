94cfa3a9674b47d9b99c4918252e2752c7b94f5a
kkufieta <katharina.kufieta@gmail.com>
2020-04-23 17:50:48 -0400

    Fix environment variables and token generation

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,1 +1,2 @@
 LOCAL_DATABASE_NAME='sealmess'
+export LOCAL_DATABASE_NAME
--- backend/src/tests/config.py
+++ backend/src/tests/config.py
@@ -2,26 +2,19 @@ import os
 import http.client
 import json
 
-customer_access_token = os.environ.get('CUSTOMER_ACCESS_TOKEN')
-provider_access_token = os.environ.get('PROVIDER_ACCESS_TOKEN')
-owner_access_token = os.environ.get('OWNER_ACCESS_TOKEN')
+# Using the manually copied access tokens
+# customer_access_token = os.environ.get('CUSTOMER_ACCESS_TOKEN')
+# provider_access_token = os.environ.get('PROVIDER_ACCESS_TOKEN')
+# owner_access_token = os.environ.get('OWNER_ACCESS_TOKEN')
 
-
+# --- 
+#   Attempted to grab access tokens automatically but
+#   is out of scope for now.
+# --- 
 domain = os.environ.get('DOMAIN')
 audience = os.environ.get('AUDIENCE')
-conn = http.client.HTTPSConnection(domain)
 
-print(domain, conn)
-
-conn = http.client.HTTPSConnection("dev-9rqh2tib.auth0.com")
-
-payload = """
-{{\"client_id\":\"{}\",
-\"client_secret\":\"{}\",
-\"audience\":\"{}\",
-\"grant_type\":
-\"client_credentials\"}}
-"""
+payload = "{{\"client_id\":\"{}\",\"client_secret\":\"{}\",\"audience\":\"{}\",\"grant_type\":\"client_credentials\"}}"
 
 headers = { 'content-type': "application/json" }
 
@@ -32,12 +25,13 @@ customer_payload = payload.format(customer_client_id,
                                   customer_client_secret,
                                   audience)
 
+conn = http.client.HTTPSConnection(domain)
 conn.request("POST", "/oauth/token", customer_payload, headers)
 res = conn.getresponse()
 data = res.read().decode("utf-8")
 data = json.loads(data)
-print(data)
 customer_access_token = data['access_token']
+conn.close()
 
 # Get the provider bearer token
 provider_client_id = os.environ.get('PROVIDER_CLIENT_ID')
@@ -45,12 +39,13 @@ provider_client_secret = os.environ.get('PROVIDER_CLIENT_SECRET')
 provider_payload = payload.format(provider_client_id,
                                   provider_client_secret,
                                   audience)
-conn.request("POST", "/oauth/token", customer_payload, headers)
+conn = http.client.HTTPSConnection(domain)
+conn.request("POST", "/oauth/token", provider_payload, headers)
 res = conn.getresponse()
 data = res.read().decode("utf-8")
 data = json.loads(data)
-print(data)
 provider_access_token = data['access_token']
+conn.close()
 
 # Get the owner bearer token
 owner_client_id = os.environ.get('OWNER_CLIENT_ID')
@@ -59,12 +54,14 @@ owner_payload = payload.format(owner_client_id,
                                owner_client_secret,
                                audience)
 
-conn.request("POST", "/oauth/token", payload, headers)
+
+conn = http.client.HTTPSConnection(domain)
+conn.request("POST", "/oauth/token", owner_payload, headers)
 res = conn.getresponse()
 data = res.read().decode("utf-8")
 data = json.loads(data)
-print(data)
 owner_access_token = data['access_token']
+conn.close()
 
 # Create the headers
 customer_header = {"Authorization": "Bearer {}".format(customer_access_token)}
